55319ca041f23f2d90c047c80386127c8ca4c0f8
ethales <emanuel@esportes.co>
2016-02-24 16:40:40 +0000

    Inclusão de vídeos nas páginas de profile dos jogadores.

--- www/js/services.js
+++ www/js/services.js
@@ -5,26 +5,26 @@ angular.module('starter.services', [])
 
   // Some fake testing data
  var players = [
-    { id:0, name: 'Kleber', data: [[1, 1, 2, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'kleber'									
-} , { id:1, name: 'Ademar', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:2, name: 'Xuxa', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:3, name: 'Igor', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:4, name: 'Jackson', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:5, name: 'Escada', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:6, name: 'Negão', data: [[1, 2, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:7, name: 'João', data: [[1, 1, 3, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:8, name: 'Gonçalo', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:9, name: 'Barba', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:10, name: 'Japonês', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:11, name: 'Ricardo', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:12, name: 'Mauro', data: [[1, 0, 2, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'mauro'									
-} , { id:13, name: 'Gil', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:14, name: 'Roberto', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:15, name: 'Caio', data: [[1, 3, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:16, name: 'Val', data: [[1, 3, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:17, name: 'Xupin', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:18, name: 'Nei', data: [[1, 1, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'									
-} , { id:19, name: 'Samuel', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil'						
+    { id:1, name: 'Ademar', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','','',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=30m12s','','','']									
+} , { id:9, name: 'Barba', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:15, name: 'Caio', data: [[1, 3, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','Vídeo 3',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=21m15s','https://youtu.be/NIxmUvblW1E?t=38m12s','https://youtu.be/NIxmUvblW1E?t=44m8s','']									
+} , { id:5, name: 'Escada', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:13, name: 'Gil', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:8, name: 'Goncalo', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:3, name: 'Igor', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:4, name: 'Jackson', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=30m12s','https://youtu.be/NIxmUvblW1E?t=47m32s','','']									
+} , { id:10, name: 'Japones', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:7, name: 'João', data: [[1, 1, 3, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','Vídeo 3','Vídeo 4'], videos_links:['https://youtu.be/NIxmUvblW1E?t=9m3s','https://youtu.be/NIxmUvblW1E?t=11m3s','https://youtu.be/NIxmUvblW1E?t=47m32s','https://youtu.be/NIxmUvblW1E?t=62m0s']									
+} , { id:0, name: 'Kleber', data: [[1, 1, 2, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'kleber', videos_nomes:['Vídeo 1','Vídeo 2','Vídeo 3',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=21m15s','https://youtu.be/NIxmUvblW1E?t=31m12s','https://youtu.be/NIxmUvblW1E?t=38m12s','']									
+} , { id:12, name: 'Mauro', data: [[1, 0, 2, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'mauro', videos_nomes:['Vídeo 1','Vídeo 2','',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=44m8s','https://youtu.be/NIxmUvblW1E?t=56m38s','','']									
+} , { id:6, name: 'Negao', data: [[1, 2, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','Vídeo 3',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=9m3s','https://youtu.be/NIxmUvblW1E?t=11m3s','https://youtu.be/NIxmUvblW1E?t=63m17s','']									
+} , { id:18, name: 'Nei', data: [[1, 1, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=55m38s','https://youtu.be/NIxmUvblW1E?t=62m0s','','']									
+} , { id:11, name: 'Ricardo', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:14, name: 'Roberto', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:19, name: 'Samuel', data: [[1, 1, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','','',''], videos_links:['https://youtu.be/NIxmUvblW1E?t=13m46s','','','']									
+} , { id:16, name: 'Val', data: [[1, 3, 1, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['Vídeo 1','Vídeo 2','Vídeo 3','Vídeo 4'], videos_links:['https://youtu.be/NIxmUvblW1E?t=13m46s','https://youtu.be/NIxmUvblW1E?t=16m12s','https://youtu.be/NIxmUvblW1E?t=56m38s','https://youtu.be/NIxmUvblW1E?t=63m17s']									
+} , { id:17, name: 'Xupin', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']									
+} , { id:2, name: 'Xuxa', data: [[1, 0, 0, 1, 1, 3]], equipe: 'Amigos de Quinta', foto: 'perfil', videos_nomes:['','','',''], videos_links:['','','','']								
 }
 ];
 
